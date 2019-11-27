dd99770d777c1dc842b3c94a79176805673adf4e
Benjamin Sonley <benjamin.sonley@unic.com>
2019-11-27 15:13:29 +0100

    add history api fallback to webpack dev server

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -10,7 +10,8 @@ module.exports = merge(common, {
     chunkFilename: "js/[name].chunk.js"
   },
   devServer: {
-    inline: true
+    inline: true,
+    historyApiFallback: true
   },
   plugins: [
     new Webpack.DefinePlugin({
